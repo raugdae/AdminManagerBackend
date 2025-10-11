82a58cb7cd4fa42ccb8997246f19f9947ba74d7b
raugdae <raugdae@gmail.com>
2025-10-11 17:59:25 +0200

    correction

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,51 +1,65 @@
-import express, { Router } from 'express';
-import {authenticateToken, authorizeRole} from '../middleware/auth.js';
-import * as adminController from '../controllers/adminController.js';
+import express, { Router } from "express";
+import { authenticateToken, authorizeRole } from "../middleware/auth.js";
+import * as adminController from "../controllers/adminController.js";
 
 const router = express.Router();
 
 router.use(authenticateToken);
-router.use(authorizeRole('admin'));
+router.use(authorizeRole("admin"));
 
-<<<<<<< HEAD
 //GET routes
-=======
 
->>>>>>> a5f5d6152e79a06531e5ed9680e610ebf7c66190
-router.get('/allEvents',adminController.getAllEvents);
-router.get('/event/:eventid/attendees',adminController.getAllAttendeeFromEvent);
-router.get('/event/:eventid/countAttendees',adminController.getCountOfAttendee);
-router.get('/event/:eventid/getAttendeeByGroup/:groupid',adminController.getAttendeeByGroup);
+router.get("/allEvents", adminController.getAllEvents);
+router.get(
+  "/event/:eventid/attendees",
+  adminController.getAllAttendeeFromEvent
+);
+router.get(
+  "/event/:eventid/countAttendees",
+  adminController.getCountOfAttendee
+);
+router.get(
+  "/event/:eventid/getAttendeeByGroup/:groupid",
+  adminController.getAttendeeByGroup
+);
 
-router.get('/person/getAllPerson',adminController.getAllPerson);
-router.get('/person/:personid/getPersonAllergens',adminController.getPersonAllergens); 
-router.get('/api/user/getAllUsers',adminController.getAllUsers);
+router.get("/person/getAllPerson", adminController.getAllPerson);
+router.get(
+  "/person/:personid/getPersonAllergens",
+  adminController.getPersonAllergens
+);
+router.get("/api/user/getAllUsers", adminController.getAllUsers);
 
-router.get('/data/getAllergenList',adminController.getAllergenList);
-
+router.get("/data/getAllergenList", adminController.getAllergenList);
 
 // POST routes
-router.post('/event/newEvent',adminController.newEvent);
-router.post('/event/:eventid/addgroup',adminController.addNewGroup);
-router.post('/event/:eventid/addChildGroup/',adminController.addNewChildGroup);
-<<<<<<< HEAD
-
-router.post('/group/addUser',adminController.addAttendeeToGroup);
-
-=======
-router.post('/group/addAttendee',adminController.addAttendeeToGroup);
->>>>>>> a5f5d6152e79a06531e5ed9680e610ebf7c66190
-router.post('/person/addPerson',adminController.addPerson);
-router.post('/person/:personid/addAllergen',adminController.addAllergenToPerson);
+router.post("/event/newEvent", adminController.newEvent);
+router.post("/event/:eventid/addgroup", adminController.addNewGroup);
+router.post("/event/:eventid/addChildGroup/", adminController.addNewChildGroup);
+router.post("/group/addUser", adminController.addAttendeeToGroup);
+router.post("/group/addAttendee", adminController.addAttendeeToGroup);
+router.post("/person/addPerson", adminController.addPerson);
+router.post("/person/:personid/addAllergen",adminController.addAllergenToPerson);
 
 //PATCH routes
-router.patch('/event/:eventid/updateInfomaniakTicketing',adminController.updateInfomaniakTicketing)
+router.patch(
+  "/event/:eventid/updateInfomaniakTicketing",
+  adminController.updateInfomaniakTicketing
+);
 
-router.patch('/person/:personid/updateProfile',adminController.updatePersonData);
-router.patch('/person/:personid/updateAllergen',adminController.updatePersonAllergen);
+router.patch(
+  "/person/:personid/updateProfile",
+  adminController.updatePersonData
+);
+router.patch(
+  "/person/:personid/updateAllergen",
+  adminController.updatePersonAllergen
+);
 
 //DELETE routes
-router.delete('/person/:personid/removeAllergen',adminController.deletePersonAlergen);
-
+router.delete(
+  "/person/:personid/removeAllergen",
+  adminController.deletePersonAlergen
+);
 
 export default router;
